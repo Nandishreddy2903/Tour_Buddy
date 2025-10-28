a95c8b92fba6f3c8f18b0dfdbd9d683b4da48741
Nandishreddy2903 <reddynandish1@gmail.com>
2025-10-28 17:13:41 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,5 @@
 //const BASE_URL = "http://localhost:3050/api";
-const BASE_URL= "https://tour-buddy-b4q3.onrender.com";
+//const BASE_URL= "https://tour-buddy-b4q3.onrender.com";
+// src/utils/baseUrl.js (for example)
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default BASE_URL;
